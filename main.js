834613b4b98dbf757a3c53d5476323258f97c937
TolnaiBalint <bliny073@gmail.com>
2023-02-21 08:27:15 +0100

    Login

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const togglePassword = button => {
+
+    button.classList.toggle("showing")
+
+    const input =
+        document.getElementById("password")
+
+    input.type =
+        input.type === "password"
+            ? "text"
+            : "password"
+}
